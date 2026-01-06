f234ea40a7998c1ce4ae52f5c278a22a19d0a3e7
tanvi-parmar <tanviparmar357@gmail.com>
2026-01-06 09:26:09 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,75 @@
+print("hello world.!!")
+#integer
+a=10
+print("a=",a)
+print("datatype of a :",type(a))
+
+#float
+b=12.5
+print("\nb=",b)
+print("datatype of b :",type(b))
+
+#string
+c="Hello Python"
+print("\nc=",c)
+print("datatype of c :",type(c))
+
+#complex
+d=1j
+print("\nd=",d)
+print("datatype of d :",type(d))
+
+#list
+e=["apple","banana","cherry"]
+print("\ne=",e)
+print("datatype of e :",type(e))
+
+#tuple
+f=("apple","banana","cherry")
+print("\nf=",f)
+print("datatype of f :",type(f))
+
+#range
+g=range(6)
+print("\ng=",g)
+print("datatypr of g :",type(g))
+
+#dict
+h={"name": "John","age":36}
+print("\nh=",h)
+print("datatypr of h :",type(h))
+
+#set
+i={"apple","banana","cherry"}
+print("\ni=",i)
+print("datatype of i :",type(i))
+
+#frozenset
+j=frozenset({"apple","banana","cherry"})
+print("\nj=",j)
+print("datatype of j :",type(j))
+
+#boolean
+k=True
+print("\nk=",k)
+print("datatype of k :",type(k))
+
+#bytes
+l=b"Hello"
+print("\nl=",l)
+print("datatype of l :",type(l))
+
+#bytearray
+m=bytearray(5)
+print("\nm=",m)
+print("datatype of m :",type(m))
+
+#memoryview
+n=memoryview(bytes(5))
+print("\nn=",n)
+print("datatype of n :",type(n))
+
+#nonetype
+o=None
+print("\no=",o)
+print("datatype of o :",type(o))
