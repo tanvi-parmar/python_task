9936e0c9861c1c5f77a63a8d6c74b453037f5ea5
tanvi-parmar <tanviparmar357@gmail.com>
2026-01-27 09:03:25 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,50 @@
+#Arithmetic operator
+a=6
+b=2
+print(a+b)
+print(a-b)
+print(a/b)
+
+#assignment opertor
+x=30
+print(x)
+
+#unary minus
+a=4
+print(-a)
+
+#relational operator
+p=10
+q=5
+print(p>q)
+print(p<q)
+print(p==q)
+
+#logical operatoer
+x=10>5
+y=5>10
+print(x and y)
+print(x or y)
+print(not x)
+
+#boolean operator
+is_pass =True
+print(is_pass)
+
+#bitwise operator
+a=5
+b=2
+print(a&b)
+print(a|b)
+print(a^b)
+
+#membership operator
+number=[1,2,3,4]
+print(2 in number)
+print(5 in number)
+
+#identity operator
+x=10
+y=10
+print(x is y)
+print(x is not y)
