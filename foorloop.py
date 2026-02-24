27c4711ca825975dbf7698db7c0b73e4c33c5854
tanvi-parmar <tanviparmar357@gmail.com>
2026-02-24 08:11:29 +0530

    Add files via upload

---
+++ foorloop.py
@@ -0,0 +1,35 @@
+for i in range(1,6):
+    print(i)
+
+for i in range (5):
+    print("hello")
+
+for i in range (1,11):
+    print(i) 
+
+for i in range (1,21): 
+    if i % 2 == 0:
+        print(i)
+
+for i in range (1,15):
+    if i % 2 != 0:
+        print(i)
+    
+for i in range (1,11):
+    print("5 x ",i," = ",5*i)
+
+name = "Atmiya"
+for letter in name :
+    print(letter) 
+
+total = 0
+for i in range (1,6):
+    total = total + i 
+    print("Sum is :" ,total)
+
+numbers = [10,20,30,40]
+for n in numbers :
+    print(n)
+
+
+    
---
+++ while.py
@@ -0,0 +1,19 @@
+i=1
+while i <=10:
+    print(i)
+    i = i + 1
+
+n=int(input("Enter value :"))
+i = 1
+s = 0
+while i <=n:
+    s = s + i
+    i = i + 1
+    print("sum = ",s)
+
+num = int(input("Enter number :"))
+i = 1
+while i <= 10:
+    print(num," x ",i," = ",num*i)
+    i = i + 1
+    
